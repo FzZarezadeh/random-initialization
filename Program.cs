3202a2e9b6c344b8ebcd636708ac63248d88e276
FzZarezadeh <fatmhzhrazarzadh@gmail.com>
2025-12-06 11:26:26 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace random_initialization
+{
+    class Program
+    {
+        static void initialization(int length,int[] arr)
+        {
+            Random x = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                arr[i] = x.Next();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter length of your array:");
+            int lengtharr = int.Parse(Console.ReadLine());
+            int[] arr = new int[lengtharr];
+            initialization(lengtharr,arr);
+            for (int i = 0; i < lengtharr; i++)
+            {
+                Console.WriteLine("the value of {0} cell: {1}",i+1,arr[i]);
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("end");
+            Console.ReadKey();
+        }
+    }
+}
